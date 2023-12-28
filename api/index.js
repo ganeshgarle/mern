b88dbd71e6cfb16a2b8f58167a41a613400e3185
ganeshgarle <garleganesh.c@gmail.com>
2023-12-28 17:53:36 +0530

    Create sign up API route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import bodyParser from "body-parser";
 
 // config env variables
 dotenv.config();
@@ -20,5 +22,7 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on 3000 !");
 });
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
