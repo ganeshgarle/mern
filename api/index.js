74a6e2c4e4dc550b9c5bd5f44956f91b858c35a9
ganeshgarle <garleganesh.c@gmail.com>
2024-01-01 12:47:32 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 
@@ -23,7 +24,7 @@ app.listen(3000, () => {
 });
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
